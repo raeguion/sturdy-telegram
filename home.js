31f1e25ca4730b0ef4f8415401ee1a90ee84b941
raeguion <hguion@artic.edu>
2026-08-26 14:45:33 -0500

    Canvas added

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
+//this is how you format comments in .js
+
 console.log("hello world!")
+
+function setup () {
+    createCanvas (windowWidth,windowHeight)
+}
+
+function draw () {
+    //background (200)
+    rectMode(CENTER)
+    rect(mouseX,mouseY,50,50)
+    
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight)
+
+}
